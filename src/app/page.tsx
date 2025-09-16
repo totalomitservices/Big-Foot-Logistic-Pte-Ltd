ebc4504b9f47e1a037cc24b0aca553be21bf6037
Ram tom <ramtom1008@gmail.com>
2025-09-16 09:16:52 +0000

    remove this section (_for element <RouteOptimizer>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/pages/home/hero';
 import Services from '@/components/pages/home/services';
 import WhyChooseUs from '@/components/pages/home/why-choose-us';
 import LogoWall from '@/components/pages/home/logo-wall';
-import RouteOptimizer from '@/components/pages/home/route-optimizer';
 import Footer from '@/components/layout/footer';
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
         <Hero />
         <Services />
         <WhyChooseUs />
-        <RouteOptimizer />
         <LogoWall />
       </main>
       <Footer />
