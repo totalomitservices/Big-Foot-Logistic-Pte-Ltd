adbe85a6ba02e8125989ed7359e70fa7b7c296a9
Ram tom <ramtom1008@gmail.com>
2025-09-16 09:39:38 +0000

    make this image size even bigger  (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-2 text-primary ${className}`}>
-      <div className="relative h-16 w-16">
+      <div className="relative h-20 w-20">
         <Image
           src="https://raw.githubusercontent.com/swathitom1207/logo-image-/main/contact%20us%202.png"
           alt="Bigfoot Logistics Logo"
