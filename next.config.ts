9907dbaf8ae51206086caaa77e852cff245e564c
Ram tom <ramtom1008@gmail.com>
2025-09-16 09:36:08 +0000

    change background image for my given link (https://github.com/swathitom1

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
