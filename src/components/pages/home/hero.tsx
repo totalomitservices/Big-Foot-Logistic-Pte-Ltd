a54a47380411c8f146d2e2677733c6d7bd411828
vigneshr tom <vigneshrtom@gmail.com>
2025-09-16 10:20:30 +0000

    alignment is ok but, position move left side

--- src/components/pages/home/hero.tsx
+++ src/components/pages/home/hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
         />
       )}
       <div className="absolute inset-0 bg-primary/70" />
-      <div className="relative z-10 flex h-full flex-col items-center justify-center text-center text-white px-4 md:px-12 lg:px-24">
+      <div className="relative z-10 flex h-full flex-col items-start justify-center text-left text-white px-4 md:px-12 lg:px-24">
         <h1 className="font-headline text-2xl md:text-4xl font-bold uppercase tracking-tight drop-shadow-lg">
           Moving Your World,
           <br />
