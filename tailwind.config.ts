6ead31a3c5c37ee2faa96ecfb5eeb80e8655c117
vigneshr tom <vigneshrtom@gmail.com>
2025-09-18 05:03:29 +0000

    change the font style : header - Poppins body - Lato

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -10,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Open Sans', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
+        body: ['Lato', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
