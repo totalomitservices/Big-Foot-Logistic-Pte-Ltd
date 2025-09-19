8ad44dc566759e860d31fd353afff1e3eb73f4a7
Kaviya <kaviyagovindharaj1209@gmail.com>
2025-09-19 06:56:40 +0000

    change this background image (https://raw.githubusercontent.com/Ram-0609

--- src/app/services/custom-clearance/page.tsx
+++ src/app/services/custom-clearance/page.tsx
@@ -17,7 +17,7 @@ export default function CustomClearancePage() {
       {/* Hero Section */}
       <section className="relative w-full h-[40vh] md:h-[50vh]">
         <Image
-          src="https://raw.githubusercontent.com/Ram-0609/Bigfoot-Logistics-Images/main/Custom-Clearance.jpg"
+          src="https://raw.githubusercontent.com/Ram-0609/Bigfoot-Logistics-Images/refs/heads/main/Custom-Clearance%20.jpeg"
           alt="Customs clearance process"
           fill
           priority
