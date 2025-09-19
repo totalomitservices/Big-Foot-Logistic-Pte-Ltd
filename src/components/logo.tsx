b5c83bd7b5d1ed4cce45627ecc622de11ffef334
Kaviya <kaviyagovindharaj1209@gmail.com>
2025-09-19 04:22:18 +0000

    add a logo header section left side corner "https://github.com/Ram-0609/

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,7 +10,7 @@ export function Logo({ className, ...props }: LogoProps) {
   return (
     <div className={cn('relative h-[140px] w-[140px]', className)}>
       <Image
-        src="https://raw.githubusercontent.com/swathitom1207/logo-image-/main/contact%20us%202.png"
+        src="https://github.com/Ram-0609/Bigfoot-Logistics-Images/blob/main/bfl_logo.png?raw=true"
         alt="Bigfoot Logistics Logo"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 140px"
