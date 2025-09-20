70f6937b2ab3d08881b05800eea83be6730b201d
Dhanusha .S <dhansiii07@gmail.com>
2025-09-20 05:28:25 +0000

    decrease the header size (_for element <HeaderClient>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,12 +8,12 @@ type LogoProps = {
 
 export function Logo({ className }: LogoProps) {
   return (
-    <div className={cn('relative w-[200px] h-[75px]', className)}>
+    <div className={cn('relative w-[160px] h-[60px]', className)}>
       <Image
         src="https://github.com/Ram-0609/Bigfoot-Logistics-Images/blob/main/bfl_logo.png?raw=true"
         alt="Bigfoot Logistics Logo"
         fill
-        sizes="(max-width: 768px) 100vw, 200px"
+        sizes="(max-width: 768px) 100vw, 160px"
         className="object-contain"
         priority
       />
