4731e1bb4bcf8f41389c1a9ba6ef3dc1af94429c
Dhanusha .S <dhansiii07@gmail.com>
2025-09-20 05:07:30 +0000

    increase size this logo as 200px (_for element img_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,20 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-          <link
-            rel="preload"
-            href="/_next/static/media/3e0eca94.p.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/_next/static/media/2af60835.p.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
       </head>
       <body className={cn(
           'font-body antialiased bg-background text-foreground',
